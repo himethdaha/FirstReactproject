fb8535fbcaf86efba3ba0fac006307cda4643267
himethdaha <himethdaha@gmail.com>
2023-04-22 17:35:22 -0400

    Add a skeleton home component and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./css/App.css";
+import React from "react";
 import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   return (
@@ -7,6 +9,9 @@ function App() {
       <header className="App-header">
         <Navbar />
       </header>
+      <main className="Main">
+        <Home />
+      </main>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,7 @@
+import "./css/Main.css";
+
+const Home = () => {
+  return <h1 className="home">HOME</h1>;
+};
+
+export default Home;
